2a17a6dff1fa511b568372e03603fe7a574b81a1
Knut Kirkhorn <knutkirk@hotmail.com>
2022-06-16 20:47:48 +0200

    Use `discord-webhook-wrapper`

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-import {MessageEmbed, WebhookClient} from 'discord.js';
+import discordWebhookWrapper from 'discord-webhook-wrapper';
+import {MessageEmbed} from 'discord.js';
 import Docker from 'dockerode';
 import config from './config.js';
 
 const docker = new Docker();
-const {discordWebhookUrl, discordWebhookId, discordWebhookToken} = config;
-
-// Check if either Discord Webhook URL or Discord Webhook ID and token is provided
-if (!(discordWebhookUrl || (discordWebhookId !== '' && discordWebhookToken !== ''))) {
-    throw new Error('You need to specify either Discord Webhook URL or both Discord Webhook ID and token!');
-}
-
-const webhookClient = discordWebhookUrl ? new WebhookClient({url: discordWebhookUrl}) : new WebhookClient({id: discordWebhookId, token: discordWebhookToken});
+const webhookClient = discordWebhookWrapper(config);
 
 const embedChatColors = {
     error: '#ea5440',
